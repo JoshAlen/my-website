c9dfa495a801ca4babd80c65ee5837f33e860626
Joshua Seitz <seitzjoshua7@gmail.com>
2017-09-30 02:34:47 +0000

    change layout on homepage, adding click animation and hover affect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$( document ).ready(function() {
+    $(".box").click(function(){
+        $(".box").removeClass("box-animation");
+        $(this).addClass("box-animation");
+    });
+});
