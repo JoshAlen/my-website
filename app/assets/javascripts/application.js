ec929db8496b23676b9337f7e347e8b77e3b0182
Joshua Seitz <seitzjoshua7@gmail.com>
2017-10-02 01:10:21 +0000

    fix quotemachine script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,41 +16,7 @@
 //= require_tree .
 
 $( document ).ready(function() {
-    
-    var quoteArr = [
 
-      "Don\'t cry because it's over, smile because it happened. ― Dr. Seuss",
-    
-      "Be yourself\; everyone else is already taken. ― Oscar Wilde",
-
-      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. ― Albert Einstein",
-
-      "So many books, so little time. ― Frank Zappa",
-
-      "A room without books is like a body without a soul. ― Marcus Tullius Cicero",
-
-      "You only live once, but if you do it right, once is enough. ― Mae West",
-
-      "Be the change that you wish to see in the world. ― Mahatma Gandhi",
-
-      "In three words I can sum up everything I've learned about life: it goes on. ― Robert Frost",
-
-      "If you tell the truth, you don't have to remember anything. ― Mark Twain",
-
-      "A friend is someone who knows all about you and still loves you. ― Elbert Hubbard"
-
-    ];
-
-    var randomNumber = 0;
-
-    function getNumber() {
-      randomNumber = Math.floor(Math.random() * 10);
-      document.getElementById("quote").innerHTML = quoteArr[randomNumber];
-    };
-
-    getNumber();
-    
-    
     $(".box").click(function(){
         $(".box").removeClass("box-animation");
         $(this).addClass("box-animation");
