e0ca59d9d5ea469a4c488bc9c0109eb413ae513f
Joshua Seitz <seitzjoshua7@gmail.com>
2017-11-11 20:06:35 +0000

    added chef signup and login path

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,7 +2,14 @@ class Chef < ActiveRecord::Base
     
     has_many :recipes
     has_many :likes
+    
     validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
-    validates :email, presence: true, length: {maximum: 105}, uniqueness: { case_sensitive: false }
-
+    
+    before_save {self.email = email.downcase }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\z/i
+    validates :email, presence: true, length: { maximum: 105}, 
+    uniqueness: { case_sensitive: false},
+    format: { with: VALID_EMAIL_REGEX }
+    
+    has_secure_password
 end
